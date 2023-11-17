0aa8e47ab7b02594415c076c14061d8cc3673e6a
KingZ060 <103080501+KingZ060@users.noreply.github.com>
2023-11-17 10:46:18 -0800

    progress save

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import "./style.css";
 import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
+import { Board } from "./board";
+import { Cell } from "./board";
 
 const MERRILL_CLASSROOM = leaflet.latLng({
   lat: 36.9995,
@@ -33,6 +35,7 @@ leaflet
   })
   .addTo(map);
 
+const board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
 const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
@@ -41,57 +44,74 @@ const sensorButton = document.querySelector("#sensor")!;
 sensorButton.addEventListener("click", () => {
   navigator.geolocation.watchPosition((position) => {
     playerMarker.setLatLng(
-      leaflet.latLng(position.coords.latitude, position.coords.longitude),
+      leaflet.latLng(position.coords.latitude, position.coords.longitude)
     );
     map.setView(playerMarker.getLatLng());
   });
 });
 
+interface Coin {
+  cell: Cell;
+  serial: number;
+}
+
+let coinMap: Map<string, Coin[]> = new Map<string, Coin[]>();
+const PLAYER_COINS: Coin[] = [];
 let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
-  const bounds = leaflet.latLngBounds([
-    [
-      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
-    ],
-    [
-      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
-    ],
-  ]);
-
+  const cell = board.getCellForPoint(
+    leaflet.latLng({
+      lat: MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+      lng: MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+    })
+  );
+  const bounds = board.getCellBounds(cell);
   const pit = leaflet.rectangle(bounds) as leaflet.Layer;
 
   pit.bindPopup(() => {
-    let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    let value = Math.floor(
+      luck([cell.i, cell.j, "initialValue"].toString()) * 100
+    );
     const container = document.createElement("div");
     container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
+                <div>There is a pit here at "${cell.i},${cell.j}". It has value <span id="value">${value}</span>.</div>
+                <div class="coinDisplay" style="height: 100px; overflow-y: scroll;"></div>
                 <button id="poke">poke</button>
                 <button id="deposit">deposit</button>`;
 
+    const coinDisplay =
+      container.querySelector<HTMLDivElement>(".coinDisplay")!;
     const poke = container.querySelector<HTMLButtonElement>("#poke")!;
     poke.addEventListener("click", () => {
       if (value <= 0) {
         alert("There no more coin to poked from this pit.");
         return;
       }
+      // const coin = coinMap.pop();
+      // playerCoins.push(coin);
       value--;
+      points++;
       container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
         value.toString();
-      points++;
       statusPanel.innerHTML = `${points} points accumulated`;
     });
+
     const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
     deposit.addEventListener("click", () => {
       if (points <= 0) return;
+      // value++;
+      // container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+      //   value.toString();
+      // points--;
+      const coin = playerCoins.pop(); // Remove a coin from the player's list
+      coinMap.push(coin); // Add the coin back to the pit
+      points--;
       value++;
       container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
         value.toString();
-      points--;
       if (points == 0) statusPanel.innerHTML = `No points yet...`;
       else statusPanel.innerHTML = `${points} points accumulated`;
     });
