02b2ea7893ba0ee2a435bd963e103a2ad579f6a4
KingZ060 <103080501+KingZ060@users.noreply.github.com>
2023-12-05 04:23:05 -0800

    Finished D3.c

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.css";
 import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
+import L from "leaflet";
 import { Board } from "./board";
 import { Cell } from "./board";
 
@@ -15,6 +16,7 @@ const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const PIT_SPAWN_PROBABILITY = 0.1;
+const MOVE_STEP = TILE_DEGREES * 2;
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
@@ -61,7 +63,7 @@ function createCoin(i: number, j: number, serial: number) {
 }
 
 function updateCoinDisplay(coinDisplay: HTMLDivElement, coins: Coin[]) {
-  coinDisplay.innerHTML = ""; // Clear existing content
+  coinDisplay.innerHTML = "";
   coins.forEach((coin, index) => {
     const coinElement = document.createElement("div");
     coinElement.textContent = `Coin ${index + 1} at (${coin.cell.i}, ${
@@ -71,10 +73,42 @@ function updateCoinDisplay(coinDisplay: HTMLDivElement, coins: Coin[]) {
   });
 }
 
-let coinMap: Map<string, Coin[]> = new Map<string, Coin[]>();
+let geocacheMap: Map<string, Geocache> = new Map<string, Geocache>();
 const playerCoins: Coin[] = [];
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
+
+interface Momento<T> {
+  toMomento(): T;
+  fromMomento(momento: T): void;
+}
+
+class Geocache implements Momento<string> {
+  i: number;
+  j: number;
+  coins: Coin[];
+  constructor(i: number, j: number, coins: Coin[]) {
+    this.i = i;
+    this.j = j;
+    this.coins = coins;
+  }
+  toMomento() {
+    return JSON.stringify({ i: this.i, j: this.j, coins: this.coins });
+  }
+
+  fromMomento(momento: string) {
+    let data = JSON.parse(momento);
+    this.i = data.i;
+    this.j = data.j;
+    this.coins = data.coins;
+  }
+}
+
+let geocaches: Geocache[] = [];
+
+function saveGeocacheStates() {
+  geocaches.map((geocache) => geocache.toMomento());
+}
 
 function makePit(i: number, j: number) {
   const cell = board.getCellForPoint(
@@ -86,18 +120,24 @@ function makePit(i: number, j: number) {
   const bounds = board.getCellBounds(cell);
   const pit = leaflet.rectangle(bounds) as leaflet.Layer;
 
-  const initialCoinCount = Math.floor(
-    luck([i, j, "initialValue"].toString()) * 100
-  );
-  let coins: Coin[] = [];
-  for (let serial = 0; serial < initialCoinCount; serial++) {
-    coins.push(createCoin(cell.i, cell.j, serial));
+  if (!geocacheMap.has(`${cell.i},${cell.j}`)) {
+    const initialCoinCount = Math.floor(
+      luck([i, j, "initialValue"].toString()) * 100
+    );
+    let coins: Coin[] = [];
+    for (let serial = 0; serial < initialCoinCount; serial++) {
+      coins.push(createCoin(cell.i, cell.j, serial));
+    }
+    const geocache = new Geocache(cell.i, cell.j, coins);
+    geocacheMap.set(`${cell.i},${cell.j}`, geocache);
+    const newGeo = new Geocache(cell.i, cell.j, coins);
+    geocaches.push(newGeo);
   }
-  coinMap.set(`${cell.i},${cell.j}`, coins);
 
   pit.bindPopup(() => {
     const container = document.createElement("div");
-    let currentCount: Coin[] = coinMap.get(`${cell.i},${cell.j}`)!;
+    let geocache = geocacheMap.get(`${cell.i},${cell.j}`);
+    let currentCount: Coin[] = geocache ? geocache.coins : [];
     container.innerHTML = `
                 <div>There is a pit here at "${cell.i},${cell.j}". It has value <span id="value">${currentCount.length}</span>.</div>
                 <div class="coinDisplay" style="height: 100px; overflow-y: scroll;"></div>
@@ -110,15 +150,14 @@ function makePit(i: number, j: number) {
 
     const poke = container.querySelector<HTMLButtonElement>("#poke")!;
     poke.addEventListener("click", () => {
-      let pitCoins = coinMap.get(`${cell.i},${cell.j}`);
-      if (pitCoins && pitCoins.length > 0) {
-        let coin: Coin | undefined = pitCoins.pop();
+      if (geocache && geocache.coins.length > 0) {
+        let coin: Coin | undefined = geocache.coins.pop();
         if (coin !== undefined) {
           playerCoins.push(coin);
           container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-            pitCoins.length.toString();
+            geocache.coins.length.toString();
           statusPanel.innerHTML = `${playerCoins.length} points accumulated`;
-          updateCoinDisplay(coinDisplay, pitCoins);
+          updateCoinDisplay(coinDisplay, geocache.coins);
         }
       } else {
         alert("There no more coin to poked from this pit.");
@@ -131,20 +170,71 @@ function makePit(i: number, j: number) {
       if (playerCoins.length <= 0) return;
       let coin: Coin | undefined = playerCoins.pop();
       if (coin !== undefined) {
-        let pitCoins = coinMap.get(`${cell.i},${cell.j}`) || [];
-        pitCoins.push(coin);
-        coinMap.set(`${cell.i},${cell.j}`, pitCoins);
+        if (!geocache) {
+          geocache = new Geocache(cell.i, cell.j, []);
+          geocacheMap.set(`${cell.i},${cell.j}`, geocache);
+        }
+        geocache.coins.push(coin);
         container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pitCoins.length.toString();
+          geocache.coins.length.toString();
         if (playerCoins.length == 0) statusPanel.innerHTML = `No points yet...`;
         else statusPanel.innerHTML = `${playerCoins.length} points accumulated`;
-        updateCoinDisplay(coinDisplay, pitCoins);
+        updateCoinDisplay(coinDisplay, geocache.coins);
       }
     });
     return container;
   });
   pit.addTo(map);
 }
+
+function regenerateCaches() {
+  map.eachLayer((layer) => {
+    if (layer !== playerMarker && !(layer instanceof L.TileLayer)) {
+      map.removeLayer(layer);
+    }
+  });
+
+  const playerPos = playerMarker.getLatLng();
+  const iStart =
+    Math.floor((playerPos.lat - MERRILL_CLASSROOM.lat) / TILE_DEGREES) -
+    NEIGHBORHOOD_SIZE;
+  const jStart =
+    Math.floor((playerPos.lng - MERRILL_CLASSROOM.lng) / TILE_DEGREES) -
+    NEIGHBORHOOD_SIZE;
+
+  for (let i = iStart; i < iStart + NEIGHBORHOOD_SIZE * 2; i++) {
+    for (let j = jStart; j < jStart + NEIGHBORHOOD_SIZE * 2; j++) {
+      if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+        makePit(i, j);
+      }
+    }
+  }
+}
+
+function movePlayer(latChange: number, lngChange: number) {
+  saveGeocacheStates();
+  const currentPos = playerMarker.getLatLng();
+  const newPos = leaflet.latLng(
+    currentPos.lat + latChange,
+    currentPos.lng + lngChange
+  );
+  playerMarker.setLatLng(newPos);
+  map.setView(newPos);
+  regenerateCaches();
+}
+
+document
+  .getElementById("north")!
+  .addEventListener("click", () => movePlayer(MOVE_STEP, 0));
+document
+  .getElementById("south")!
+  .addEventListener("click", () => movePlayer(-MOVE_STEP, 0));
+document
+  .getElementById("east")!
+  .addEventListener("click", () => movePlayer(0, MOVE_STEP));
+document
+  .getElementById("west")!
+  .addEventListener("click", () => movePlayer(0, -MOVE_STEP));
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
